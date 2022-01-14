910986e50e5c8133d7a4dc5f146655ac019db25f
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-14 09:17:05 +0700

    create view file

---
+++ src/view/TodolistView.java
@@ -0,0 +1,20 @@
+package view;
+
+public class TodolistView {
+
+    // Main Menu
+    public void showTodolist() {
+
+    }
+
+    // View to add Todolist
+    public void addTodolist() {
+
+    }
+
+    // View to remove Todolist
+    public void removeTodolist() {
+
+    }
+
+}
