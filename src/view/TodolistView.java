8a8a0e7f63dc9c1213046705087094a07f11a858
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-14 14:12:27 +0700

    add view showTodolist

--- src/view/TodolistView.java
+++ src/view/TodolistView.java
@@ -1,10 +1,39 @@
 package view;
 
+import entity.Todolist;
+import service.TodolistService;
+import util.InputUtil;
+
 public class TodolistView {
+
+    private TodolistService todolistService;
+
+    public TodolistView(TodolistService todolistService) {
+        this.todolistService = todolistService;
+    }
 
     // Main Menu
     public void showTodolist() {
+        while (true) {
+            todolistService.showTodolist();
 
+            System.out.println("Menu");
+            System.out.println("1. Add Todo");
+            System.out.println("1. Remove Todo");
+            System.out.println("(x) to exit application.");
+
+            var input = InputUtil.input("Choose Menu");
+
+            if (input.equals("1")) {
+                addTodolist();
+            } else if (input.equals("2")) {
+                removeTodolist();
+            } else if (input.equals("x")) {
+                break;
+            } else {
+                System.out.println("Unknown Input!");
+            }
+        }
     }
 
     // View to add Todolist
