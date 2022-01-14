2cafa76f16ff051681a415fbb7864fc69290c15d
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-01-14 09:14:40 +0700

    add service

---
+++ src/service/TodolistService.java
@@ -0,0 +1,10 @@
+package service;
+
+public interface TodolistService {
+
+    // Business Logic
+    void showTodolist();
+    void addTodolist(String todo);
+    void removeTodolist(Integer number);
+
+}
